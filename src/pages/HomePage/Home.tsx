7cc0004dea996e0389ff427a8a099db419a7c4ee
MatildaAK <matilda.kallstrom@hotmail.se>
2024-12-20 12:02:41 +0100

    added intro on home page

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,9 +1,13 @@
 import About from "../../components/Home/About";
+import Intro from "../../components/Home/Intro";
 
 const Home = () => {
-    return (
-        <About />
-    )
-}
+  return (
+    <>
+      <Intro />
+      <About />
+    </>
+  );
+};
 
 export default Home;
