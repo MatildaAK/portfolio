35d13fc62d4705156c8e9b33fff7ab59ca7ab1e9
MatildaAK <matilda.kallstrom@hotmail.se>
2025-01-07 15:29:49 +0100

    added experience and education

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -12,20 +12,41 @@ const About = () => {
           <div className="flex flex-col md:flex-row">
             <div className="flex justify-center my-10 md:w-1/3">
               <img src={profileImg} alt="" className="h-40" />
-            </div>
+            </div> 
             <div className="md:w-2/3">
-              <div className="flex justify-center gap-x-6">
+            <div className="flex flex-wrap justify-center gap-6 mb-10">
+
                 <div className="border border-2 border-black dark:border-white solid rounded-lg w-1/3 py-2 text-center">
                   <p className="font-semibold text-lg">Experience</p>
                   <p>Grocery store</p>
                   <p>11 years of employee</p>
                 </div>
                 <div className="border border-2 border-black dark:border-white solid rounded-lg w-1/3 py-2 text-center">
+                  <p className="font-semibold text-lg">Experience</p>
+                  <p>Academic Work</p>
+                  <p>Junior developer</p>
+                </div>
+              </div>
+
+              <div className="flex flex-wrap justify-center gap-6">
+                <div className="border border-2 border-black dark:border-white solid rounded-lg w-1/3 py-2 text-center">
                   <p className="font-semibold text-lg">Education</p>
                   <p>Chas Academy</p>
                   <p>Fullstack Webdeveloper Open Source</p>
                 </div>
+                <div className="border border-2 border-black dark:border-white solid rounded-lg w-1/3 py-2 text-center">
+                  <p className="font-semibold text-lg">Education</p>
+                  <p>Chas Academy</p>
+                  <p>IT security for developers</p>
+                  <p>Chas Upskill</p>
+                </div>
+                <div className="border border-2 border-black dark:border-white solid rounded-lg w-1/3 py-2 text-center">
+                  <p className="font-semibold text-lg">Education</p>
+                  <p>Brights</p>
+                  <p>JAVA developer</p>
+                </div>
               </div>
+              
               <div className="flex justify-center mt-6">
                 <div className="w-[75%]">
                   <p>
