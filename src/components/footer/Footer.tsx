c794594bd54909bf4c78bcb6abe651be5d399f8a
MatildaAK <matilda.kallstrom@hotmail.se>
2025-01-07 15:05:29 +0100

    footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import {
-  faInstagram,
   faLinkedin,
+  faSquareGithub,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -10,18 +10,18 @@ const Footer = () => {
       <div className=" font-sans flex justify-center">
         <div className="flex justify-between gap-x-10 py-4 md:gap-x-20">
           <a
-            href="#"
-            className="md:cursor-pointer"
-            aria-label="Facebook ikon"
+            href="https://linkedin.com/in/matilda-agerstig-källström"
+            className="md:cursor-pointer text-blue-600 dark:text-sky-700"
+            aria-label="LinkedIn ikon"
           >
-            <FontAwesomeIcon icon={faLinkedin} size="xl" />
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
           </a>
           <a
-            href="#"
-            className="md:cursor-pointer"
-            aria-label="Instagram ikon"
+            href="https://github.com/MatildaAK"
+            className="md:cursor-pointer text-blue-600 dark:text-sky-700"
+            aria-label="Github ikon"
           >
-            <FontAwesomeIcon icon={faInstagram} size="xl" />
+            <FontAwesomeIcon icon={faSquareGithub} size="2xl" />
           </a>
         </div>
       </div>
